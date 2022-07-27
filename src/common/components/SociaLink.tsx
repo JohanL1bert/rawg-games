c0a4a7a9e0b347472b13f37c1359e21f40bb56b1
Johan <grimmerw968@gmail.com>
2022-07-27 13:11:08 +0300

    feat: add socialLink component

--- src/common/components/SociaLink.tsx
+++ src/common/components/SociaLink.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { LogInConst } from 'common/const/LogIn.const';
+import { ILogInConst } from 'common/interfaces/signLoginInterface';
 
 export const SociaLink = () => {
   return (
     <div>
       <div>
-        <div>
-          <img src="#" alt="" />
-        </div>
-        <div>
-          <span>Text</span>
-        </div>
+        {LogInConst.map((el: ILogInConst) => (
+          <i key={el.id}>
+            <div>
+              <img src={el.img} alt={el.alt} />
+            </div>
+            <div>
+              <span>{el.title}</span>
+            </div>
+          </i>
+        ))}
       </div>
     </div>
   );
