7cc3f137f13e9ae1126d83ef4bf256088cccd85d
Johan <grimmerw968@gmail.com>
2022-07-27 12:56:16 +0300

    feat: add header to page home

---
+++ src/common/components/home/HomeHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const HomeHeader = () => {
+  return (
+    <div>
+      <div>
+        <h2>New and trending</h2>
+        <p>Based on player counts and realse date</p>
+      </div>
+    </div>
+  );
+};
