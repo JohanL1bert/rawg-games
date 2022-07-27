c15efc764f20feba3b5b9cda29da74c369f0e51b
Johan <grimmerw968@gmail.com>
2022-07-27 13:00:45 +0300

    feat: add home options

---
+++ src/common/components/home/HomeOptions.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const HomeOptions = () => {
+  return (
+    <div>
+      <div>
+        <div className="">
+          <div className="">order</div>
+          <div>platforms</div>
+        </div>
+      </div>
+    </div>
+  );
+};
