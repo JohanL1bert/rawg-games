d5070a74eb3a70fb806c4ff2f60745b2d2552d04
Johan <grimmerw968@gmail.com>
2022-07-28 16:56:12 +0300

    feat: add interface implements to gameController, add start/stop bool

--- src/store/allGames/gameStore.controller.ts
+++ src/store/allGames/gameStore.controller.ts
@@ -1,38 +1,25 @@
-/* eslint-disable consistent-return */
 /* eslint-disable no-empty-function */
 /* eslint-disable no-useless-constructor */
-/* eslint-disable class-methods-use-this */
-
 import { GameStoreServices } from 'store/allGames/gameStore.services';
 import { apiService } from 'store/allGames//apiService';
-import { AppLoadStatus } from 'common/enums/store-load-status.enum';
+import { IGameController } from 'store/allGames/interface';
 
-export class GameStoreController {
+export class GameStoreController implements IGameController {
   constructor(private readonly gameStoreServices: GameStoreServices) {}
 
   key = process.env.REACT_APP_API_KEY;
 
   async uploadController(pageNumber: any) {
     try {
-      this.gameStoreServices.loadService(AppLoadStatus.pending);
+      this.gameStoreServices.startetService(true);
       const data: any = await apiService.get(
         `https://api.rawg.io/api/games?key=${this.key}&page=${pageNumber}`
       );
       this.gameStoreServices.getGameService(data.data);
-      this.gameStoreServices.loadService(AppLoadStatus.done);
     } catch (e) {
       console.log(e);
-    }
-  }
-
-  async countController() {
-    try {
-      this.gameStoreServices.loadService(AppLoadStatus.pending);
-      const count: any = await apiService.get(`https://api.rawg.io/api/games?key=${this.key}`);
-      this.gameStoreServices.getCountService(count);
-      this.gameStoreServices.loadService(AppLoadStatus.done);
-    } catch (e) {
-      console.log(e);
+    } finally {
+      this.gameStoreServices.startetService(false);
     }
   }
 }
