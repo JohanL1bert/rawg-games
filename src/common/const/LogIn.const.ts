d35518f30212badbd653c9a9da468ce95dd47615
Johan <grimmerw968@gmail.com>
2022-07-29 20:11:28 +0300

    feat: add const for login

--- src/common/const/LogIn.const.ts
+++ src/common/const/LogIn.const.ts
@@ -3,15 +3,18 @@ export const LogInConst = [
     id: 1,
     img: '',
     title: 'Continue with FaceBook',
+    alt: 'Facebook image',
   },
   {
     id: 2,
     img: '',
     title: 'Continue with Twitter',
+    alt: 'Twitter image',
   },
   {
     id: 3,
     img: '',
     title: 'Continue with Steam',
+    alt: 'Steam image',
   },
 ];
