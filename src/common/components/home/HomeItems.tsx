29628a9c2d18a248c9329b74ee8b517247ec1389
Johan <grimmerw968@gmail.com>
2022-07-29 20:10:08 +0300

    feat: add HomeSlider, Rating components

--- src/common/components/home/HomeItems.tsx
+++ src/common/components/home/HomeItems.tsx
@@ -1,29 +1,36 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable camelcase */
 import React from 'react';
+import { toJS } from 'mobx';
 import { ICardItem } from 'common/interfaces/homeItemInterface';
 import { Rating } from 'common/components/home/Rating';
+import { HomeSlider } from 'common/components/home/HomeSlider';
+import { HomeVideo } from 'common/components/home/HomeVide';
 
 export const HomeItems = ({ data }: { data: ICardItem }) => {
+  const { metacritic, name, short_screenshots, clip, background_image } = data;
+
+  console.log(toJS(data));
   return (
     <div className="home-item">
       <div className="home-item-wrapper">
         <div className="home-item-upper">
           <div className="upper__wrapper">
-            <div className="upper__play-btn" />
-            <img src={data.background_image} alt="game background" className="upper__image" />
-            <video className="upper__video" />
-            <div className="upper__play-text">Play full video</div>
+            {clip === null ? (
+              <HomeSlider shortScreenshots={short_screenshots} backgroundImage={background_image} />
+            ) : (
+              <HomeVideo />
+            )}
           </div>
         </div>
         <div className="home-item-bottom">
           <div className="bottom__wrapper">
             <div className="bottom__icon" />
-            {data.metacritic && <Rating metacritic={data.metacritic} />}
+            {data.metacritic && <Rating metacritic={metacritic} />}
             <div className="bottom__text">
               <div className="bottom__text-wrapper">
                 <p className="bottom__text-tittle">
-                  {data.name}
+                  {name}
                   <span className="bottom__text-icon" />
                 </p>
               </div>
