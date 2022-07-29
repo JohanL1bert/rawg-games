06d058430d0fe97b7b040ae9fce5c1bdde3c9b21
Johan <grimmerw968@gmail.com>
2022-07-29 20:13:49 +0300

    feat: add loginLink component

--- src/common/components/login/LoginLink.tsx
+++ src/common/components/login/LoginLink.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { AppRoute } from 'common/const/AppRoutes.const';
 
 export const LoginLink = () => {
   return (
     <div>
-      <NavLink to="/sign-in">Don&apos;t have an account? Sign ip. </NavLink>
+      <NavLink to={AppRoute.signIn}>Don&apos;t have an account? Sign ip. </NavLink>
       <NavLink to="/password_recovery">Forgot your password?</NavLink>
     </div>
   );
