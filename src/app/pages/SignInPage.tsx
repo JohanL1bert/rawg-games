3bcc654dd51e723141dd12fedc1b1a3e212a3b32
Johan <grimmerw968@gmail.com>
2022-07-29 20:11:04 +0300

    feat: add SignInPage component

--- src/app/pages/SignInPage.tsx
+++ src/app/pages/SignInPage.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { SociaLink } from 'common/components/SociaLink';
+import { SignInForm } from 'common/components/sign-in/SignInForm';
+import { SignInLink } from 'common/components/sign-in/SignInLink';
 
 export const SignInPage = () => {
-  return <div />;
+  return (
+    <section>
+      <div className="wrapper">
+        <div className="sign-in-wrapper">
+          <h2 className="sign-in-header">Sign up</h2>
+          <SignInForm />
+          <SignInLink />
+        </div>
+        <div className="social__wrapper">
+          <div className="social">
+            <SociaLink />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
